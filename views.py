77b70963fe394330577f342c6404006bdf7e6b9f
Leo Suarez <leo@dtraleigh.com>
2020-05-20 16:00:27 -0400

    Broke out html files into base and individual page files.

--- views.py
+++ views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.contrib.gis.db.models.functions import AsGeoJSON
 import json
 from django.core.serializers import serialize
-from develop.models import CitizenAdvisoryCouncil
+from develop.models import CitizenAdvisoryCouncil, TrackArea
 
 
 def cac(request):
-    # cac2 = CitizenAdvisoryCouncil.objects.annotate(json=AsGeoJSON('geom')).get(name="Central").json
-    # cac2_json = json.loads(cac2)
-    cac2 = serialize("geojson", CitizenAdvisoryCouncil.objects.all(), geometry_field="geom", fields=("name",))
+    cac_data = serialize("geojson", CitizenAdvisoryCouncil.objects.all(), geometry_field="geom", fields=("name",))
 
-    return render(request, 'index.html', {"data": cac2})
+    return render(request, "cac.html", {"cac_data": cac_data})
+
+
+def itb(request):
+    itb_data = serialize("geojson", TrackArea.objects.all(), geometry_field="geom", fields=("long_name",))
+
+    return render(request, "itb.html", {"itb_data": itb_data})
