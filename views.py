74f6f18761fedb25482c593c5692ec84c68bda71
Leo Suarez <leo@dtraleigh.com>
2020-05-20 16:14:09 -0400

    Filtered the cac map to only the ones being covered.

--- views.py
+++ views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.contrib.gis.db.models.functions import AsGeoJSON
 import json
 from django.core.serializers import serialize
-from develop.models import CitizenAdvisoryCouncil, TrackArea
+from develop.models import CitizenAdvisoryCouncil, TrackArea, coverArea
 
 
 def cac(request):
-    cac_data = serialize("geojson", CitizenAdvisoryCouncil.objects.all(), geometry_field="geom", fields=("name",))
+    itb_cacs = coverArea.objects.get(name="Downtown")
+    cac_data = serialize("geojson", itb_cacs.CACs.all(), geometry_field="geom", fields=("name",))
 
     return render(request, "cac.html", {"cac_data": cac_data})
 
