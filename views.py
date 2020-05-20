5f253051e415375dd793c112012e8b4ae07f2e11
Leo Suarez <leo@dtraleigh.com>
2020-05-19 21:22:05 -0400

    Simple example of mapping the geom data on a leaflet map.

--- views.py
+++ views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.gis.db.models.functions import AsGeoJSON
+import json
+from django.core.serializers import serialize
+from develop.models import CitizenAdvisoryCouncil
 
 
 def cac(request):
-    return render(request, 'index.html')
+    # cac2 = CitizenAdvisoryCouncil.objects.annotate(json=AsGeoJSON('geom')).get(name="Central").json
+    # cac2_json = json.loads(cac2)
+    cac2 = serialize("geojson", CitizenAdvisoryCouncil.objects.all(), geometry_field="geom", fields=("name",))
+
+    return render(request, 'index.html', {"data": cac2})
