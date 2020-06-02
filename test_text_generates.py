7d6f7f405d131a1dd416993494afce448c89bcd0
Leo Suarez <leo@dtraleigh.com>
2020-06-01 20:41:52 -0400

    Added tests to text generates.

--- test_text_generates.py
+++ test_text_generates.py
@@ -44,3 +44,75 @@ class TextGenTestCaseDjango(TestCase):
                 self.assertNotEqual(add_debug_text(item), "")
             else:
                 self.assertEqual(add_debug_text(item), "")
+
+    def test_get_new_dev_text(self):
+        new_dev1 = DevelopmentPlan.objects.all()[0]
+        new_dev2 = SiteReviewCases.objects.all()[0]
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_new_dev_text(new_dev1), None)
+        self.assertNotEqual(get_new_dev_text(new_dev2), None)
+
+    def test_get_updated_dev_text(self):
+        new_dev1 = DevelopmentPlan.objects.all()[0]
+        new_dev2 = SiteReviewCases.objects.all()[0]
+
+        # Need to perform some update on each
+        new_dev1.status = "New Status"
+        new_dev1.save()
+        new_dev2.status = "New Status"
+        new_dev2.save()
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_updated_dev_text(new_dev1), None)
+        self.assertNotEqual(get_updated_dev_text(new_dev2), None)
+
+    def test_get_new_zon_text(self):
+        new_zon1 = Zoning.objects.all()[0]
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_new_zon_text(new_zon1), None)
+
+    def test_get_updated_zon_text(self):
+        new_zon1 = Zoning.objects.all()[0]
+
+        # Need to perform some update on each
+        new_zon1.status = "New Status"
+        new_zon1.save()
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_updated_zon_text(new_zon1), None)
+
+    def test_get_new_aad_text(self):
+        new_aad1 = AdministrativeAlternates.objects.all()[0]
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_new_aad_text(new_aad1), None)
+
+    def test_get_updated_aad_text(self):
+        new_aad1 = AdministrativeAlternates.objects.all()[0]
+
+        # Need to perform some update on each
+        new_aad1.status = "New Status"
+        new_aad1.save()
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_updated_aad_text(new_aad1), None)
+
+    def test_get_new_tc_text(self):
+        new_tcc1 = TextChangeCases.objects.all()[0]
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_new_tc_text(new_tcc1), None)
+
+    def test_get_updated_tc_text(self):
+        new_tcc1 = TextChangeCases.objects.all()[0]
+
+        # Need to perform some update on each
+        new_tcc1.status = "New Status"
+        new_tcc1.save()
+
+        # We just want to know we don't get None or an assertionError when we run through these functions
+        self.assertNotEqual(get_updated_tc_text(new_tcc1), None)
+
+
